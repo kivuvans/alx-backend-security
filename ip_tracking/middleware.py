ae383f195c3d20d0ab50ae3ade2f96fd17c780ee
evans kivuva <kivuvaevans@gmail.com>
2026-01-15 15:12:58 +0300

    feat: add IP logging middleware and request log model

---
+++ ip_tracking/middleware.py
@@ -0,0 +1,25 @@
+from .models import RequestLog
+from django.utils.timezone import now
+
+
+class IPLoggingMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        ip_address = self.get_client_ip(request)
+        path = request.path
+
+        RequestLog.objects.create(
+            ip_address=ip_address,
+            path=path
+        )
+
+        response = self.get_response(request)
+        return response
+
+    def get_client_ip(self, request):
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            return x_forwarded_for.split(',')[0]
+        return request.META.get('REMOTE_ADDR')
